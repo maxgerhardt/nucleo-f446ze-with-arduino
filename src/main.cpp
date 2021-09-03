eb50e7f3ea758a40442421e416c64c315a92fcf1
unknown <maximilian.gerhardt@rub.de>
2021-09-03 22:17:39 +0200

    Add octopus variant

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,22 @@
 #define BUTTON1  PC13  // USER_Btn [B1]
 
 // select LED to blink
+#ifdef OCTOPUS_BOARD
+/* warning: PA13 is the LED on the board, but als SWDIO for programming */
+/* re-using this GPIO might result in needing to connect NRST to the SWD programmer */
+/* or pressing reset every time.. */
+#define LED PA13
+/* use USB serial :) */
+#define SERIAL_TO_USE SerialUSB
+#else 
+/* nucleo */
 #define LED LED1
+#define SERIAL_TO_USE Serial
+#endif
 
 void setup() {
 	pinMode(LED, OUTPUT);
-	Serial.begin(9600);
+	SERIAL_TO_USE.begin(9600);
 }
 
 void loop() {
@@ -20,12 +31,57 @@ void loop() {
 	delay(500);
 	digitalWrite(LED, LOW);
 	delay(500);
-	Serial.println("Blinky");
+	SERIAL_TO_USE.println("Blinky");
 }
 
-/* supply explicit clock init function. To 168MHz from 8MHz HSE. */ 
+/* supply explicit clock init function. To 168MHz from 8MHz HSE Bypass (for nucleo) or 12MHz HSE (Octopus) */ 
 extern "C" void SystemClock_Config(void)
 {
+#ifdef OCTOPUS_BOARD
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 6;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLRCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_CLK48;
+  PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48CLKSOURCE_PLLQ;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+#else
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
@@ -69,4 +125,5 @@ extern "C" void SystemClock_Config(void)
   {
     Error_Handler();
   }
+#endif
 }
